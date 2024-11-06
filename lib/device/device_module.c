d1a6d1a8b65f7ae35a0acc8b388474d3bbf0bde3
LAPTOP-NLT9GAPL\ASUS <duybach2808@gmail.com>
2024-11-07 00:52:06 +0700

    INIT DEVICE MODULE

---
+++ lib/device/device_module.c
@@ -0,0 +1,9 @@
+#include <device_module.h>
+
+//IMPLEMENT VARIALE
+int b; //EXMPLE
+
+//RETURN VARIABLE;
+int getB(){
+    return b;
+} //EXAMPLE
---
+++ lib/device/device_module.h
@@ -0,0 +1,21 @@
+#ifndef device_module_h
+#define device_module_h
+
+#include <global.h>
+// MORE INCLUDE
+
+// DEFINE VARIABLE
+# define a 10 // EXAMPLE
+
+//IMPLEMENT VARIALE
+extern int b; //EXMPLE
+
+//RETURN VARIABLE;
+void getB(); //EXAMPLE
+
+//FUNTION IMPLEMENT 
+
+
+
+
+#endif
